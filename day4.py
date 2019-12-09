626986b96fb1f3bd752ef7af048f5e310b53ec0c
Chris Boddy <chris@boddy.im>
2019-12-09 20:52:28 +0000

    day 4 part 2

--- day4.py
+++ day4.py
@@ -1,12 +1,15 @@
 import math
+from typing import *
 
 INPUT_RANGE = range(246515, 739105)
 
 
-def is_valid(x: int):
-    nums = [int(x//math.pow(10, pos) % 10)
+def _nums(x: int):
+    return [int(x//math.pow(10, pos) % 10)
             for pos in range(5, -1, -1)]
 
+
+def pt1_valid(nums: List[int]):
     has_adjacent = False
     for l, r in zip(nums[:-1], nums[1:]):
         if l == r:
@@ -17,8 +20,25 @@ def is_valid(x: int):
     return has_adjacent
 
 
+def pt2_valid(nums: List[int]):
+    has_adjacent = False
+    for idx, (l, r) in enumerate(zip(nums[:-1], nums[1:])):
+        if l == r and not has_adjacent:
+            has_adjacent = True
+            if idx > 0 and nums[idx-1] == l:
+                has_adjacent = False
+            if idx < len(nums) - 2 and nums[idx+2] == l:
+                has_adjacent = False
+        if l > r:
+            # has descending
+            return False
+    return has_adjacent
+
+
 def day4():
-    print(sum(is_valid(i)
+    print(sum(pt1_valid(_nums(i))
+              for i in INPUT_RANGE))
+    print(sum(pt2_valid(_nums(i))
               for i in INPUT_RANGE))
 
 
