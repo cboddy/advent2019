2e2baf56d85668ce952955a6c0eb6420e4c0ca8e
Chris Boddy <chris@boddy.im>
2019-12-14 14:27:30 +0000

    day6 pt2

--- day6.py
+++ day6.py
@@ -5,6 +5,8 @@ import utils
 
 _ROOT = 'COM'
 
+
+
 def parse_line(line: str) -> Tuple[str, str]:
     split = line.strip().split(')')
     assert len(split) == 2
@@ -29,6 +31,22 @@ def _dfs(adjacency_list: Dict[str, List[str]],
             _dfs(adjacency_list, child, depth+1, func)
     func(node, depth)
 
+def djikstra(start: str, end: str, adjacency_list: Dict[str, List[str]]) -> List[str]:
+    shortest_paths = collections.defaultdict(lambda: 9e99)
+    shortest_paths[start] = 0
+    to_visit = {start}
+    processed = set() 
+    while len(to_visit):
+        cur = to_visit.pop()
+        for child in adjacency_list[cur]:
+            shortest = shortest_paths[child]
+            cur_path = shortest_paths[cur] + 1
+            if cur_path < shortest:
+                shortest_paths[child] = cur_path
+            if child not in processed:
+                to_visit.add(child)
+        processed.add(cur)
+    print(shortest_paths[end]-2)
 
 def day6_pt1(lines: List[str]):
     edges = [parse_line(l) for l in lines]
@@ -41,5 +59,15 @@ def day6_pt1(lines: List[str]):
     _dfs(adjacency_list, _ROOT, 0, func)
     print(total_orbits[0])
 
+def day6_pt2(lines: List[str]):
+    edges = [parse_line(l) for l in lines]
+    bi_edges = [ee for e in edges
+                for ee in [e, (e[1], e[0])] 
+                ]
+
+    adjacency_list = to_adjacency_list(bi_edges)
+    djikstra('YOU', 'SAN', adjacency_list)
 
 utils.run(day6_pt1, 'day6_pt1.txt')
+utils.run(day6_pt2, 'day6_pt2.txt')
+
