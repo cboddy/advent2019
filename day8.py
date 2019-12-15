0c4a6352e32c72f5b579c410df7e577d214b3461
Chris Boddy <chris@boddy.im>
2019-12-15 15:14:15 +0000

    day  8 pt 2

--- day8.py
+++ day8.py
@@ -19,4 +19,18 @@ def day8_pt1(line: str):
     print(target)
 
 
+def day8_pt2(line: str):
+    line = [int(i) for i in line.strip()]
+    final_image = [2]*AREA
+    for pos in range(0, len(line), AREA):
+        layer = line[pos: pos+AREA]
+        for idx, value in enumerate(layer):
+            if final_image[idx] == 2 and value != 2:
+                final_image[idx] = value
+
+    for pos in range(0, len(final_image), WIDTH):
+        print(''.join('*' if v else ' ' for v in final_image[pos:pos+WIDTH]))
+
+
 day8_pt1(utils._read_one('day8.txt'))
+day8_pt2(utils._read_one('day8.txt'))
